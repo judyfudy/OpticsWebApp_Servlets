d24e79ba38cfd3e4f87df3037a3bda25fdf9f66f
Danylo Bubniy <43762898+judyfudy@users.noreply.github.com>
2020-01-26 19:20:00 +0200

    Add entity worker

---
+++ src/main/java/com/bubnii/entity/Worker.java
@@ -0,0 +1,98 @@
+package com.bubnii.entity;
+
+import java.util.Objects;
+
+public class Worker {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private int age;
+    private int experience;
+    private String photo;
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker [" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", age=" + age +
+                ", experience=" + experience +
+                ", photo=" + photo +
+                ']';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Worker worker = (Worker) o;
+        return id == worker.id &&
+                Objects.equals(firstName, worker.firstName) &&
+                Objects.equals(lastName, worker.lastName) &&
+                Objects.equals(middleName, worker.middleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, middleName);
+    }
+}
